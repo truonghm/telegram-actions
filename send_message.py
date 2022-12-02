ca5d18b5c17b8a530f9db0e0cc344fe15b4118ce
truonghm <truong173@outlook.com>
2022-12-02 15:28:00 +0700

    add format message

--- send_message.py
+++ send_message.py
@@ -6,7 +6,7 @@ load_dotenv()
 def send_msg(text):
 	token = os.getenv("TELEGRAM_TOKEN")
 	chat_id = os.getenv("TELEGRAM_TO")
-	url_req = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}"
+	url_req = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&parse_mode=HTML&text={text}"
 	results = requests.get(url_req)
 	print(results.json())
 
@@ -14,8 +14,10 @@ def send_msg(text):
 
 if __name__ == "__main__":
 	message = """
-	Good Morning! Don't forget these following items for the day:\n
-	- [Leedcode 75](https://leetcode.com/study-plan/leetcode-75/?progress=xdaigrwd)\n
-	- [Advent of Code 2022](https://adventofcode.com/)
+	<b>Good Morning</b> <a href="tg://user?id=hmtrg">Truong</a>! Don't forget these following items for the day:\n
+	
+	- <a href="https://leetcode.com/study-plan/leetcode-75/?progress=xdaigrwd">Leedcode 75</a>
+	- <a href="https://adventofcode.com/">Advent of Code 2022</a>
+
 	"""
 	send_msg(message)
