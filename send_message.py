539e492f9701b08bb11c02c18317db6fe1e0b279
truonghm <truong173@outlook.com>
2022-12-02 15:22:30 +0700

    update message and schedule

--- send_message.py
+++ send_message.py
@@ -6,12 +6,16 @@ load_dotenv()
 def send_msg(text):
 	token = os.getenv("TELEGRAM_TOKEN")
 	chat_id = os.getenv("TELEGRAM_TO")
-	url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
-	print(url_req)
+	url_req = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}"
 	results = requests.get(url_req)
 	print(results.json())
 
 
 
 if __name__ == "__main__":
-	send_msg("Hello there!")
+	message = """
+	Good Morning! Don't forget these following items for the day:\n
+	- [Leedcode 75](https://leetcode.com/study-plan/leetcode-75/?progress=xdaigrwd)\n
+	- [Advent of Code 2022](https://adventofcode.com/)
+	"""
+	send_msg(message)
