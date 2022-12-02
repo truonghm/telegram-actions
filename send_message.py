76b742937540e158533457f9819faff331df077e
truonghm <truong173@outlook.com>
2022-12-02 15:39:09 +0700

    update message

--- send_message.py
+++ send_message.py
@@ -6,18 +6,28 @@ load_dotenv()
 def send_msg(text):
 	token = os.getenv("TELEGRAM_TOKEN")
 	chat_id = os.getenv("TELEGRAM_TO")
-	url_req = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&parse_mode=HTML&text={text}"
-	results = requests.get(url_req)
+	parameters = {
+		'chat_id':chat_id,
+		'disable_web_page_preview':True,
+		'parse_mode':'HTML',
+		'text':text,
+	}
+	base_url = f"https://api.telegram.org/bot{token}/sendMessage?"
+	full_url = base_url + "&".join([f"{k}={v}" for k, v in parameters]) 
+	results = requests.get(full_url)
 	print(results.json())
 
 
 
 if __name__ == "__main__":
 	message = """
-	<b>Good Morning</b> <a href="tg://user?id=hmtrg">Truong</a>! Don't forget these following items for the day:\n
-	
+	<b>Good Morning</b> <a href="tg://user?id=hmtrg">Truong</a>! Don't forget these following items for the day:
+
 	- <a href="https://leetcode.com/study-plan/leetcode-75/?progress=xdaigrwd">Leedcode 75</a>
 	- <a href="https://adventofcode.com/">Advent of Code 2022</a>
 
+	For news feeds:
+	- <a href="https://news.ycombinator.com/news">HackerNews</a>
+	- <a href="https://sports.yahoo.com/nba/scoreboard/">Today's NBA Games </a>
 	"""
 	send_msg(message)
